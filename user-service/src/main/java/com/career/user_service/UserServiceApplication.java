9f704b1304c4c7405e554c09cf7530a372cd7d8a
Sadiq Pasha Shaik <sadiqpasha7474@gmail.com>
2026-06-04 15:17:43 +0530

    Updated main class to remove exclusion of DB connectors

--- user-service/src/main/java/com/career/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/career/user_service/UserServiceApplication.java
@@ -3,12 +3,8 @@ package com.career.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-    exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-    }
-)
+@SpringBootApplication  
+
 public class UserServiceApplication {
 
     public static void main(String[] args) {
